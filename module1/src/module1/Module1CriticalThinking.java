7b2a1e26fc0405c0070344145e81ff84cacfea42
egonzalez737 <edgardo.gonzalez@csuglobal.edu>
2025-08-24 23:26:31 -0400

    Add files via upload

---
+++ module1/src/module1/Module1CriticalThinking.java
@@ -0,0 +1,17 @@
+package module1;
+
+public class Module1CriticalThinking {
+	public static void main (String [] args) { 
+		String FirstName = "Master";
+		String LastName = "Chief";
+		String StreetAddress = "343 Industries Way";
+		String City = "Halo";
+		String ZipCode = "00343";
+		
+		System.out.println(FirstName);
+		System.out.println(LastName);
+		System.out.println(StreetAddress);
+		System.out.println(City);
+		System.out.println(ZipCode);
+	}
+}
